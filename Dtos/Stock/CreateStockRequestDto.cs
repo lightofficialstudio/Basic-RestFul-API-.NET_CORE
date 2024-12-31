aa2bb5be16fff06ae1b753eaa6194f1ca6e391a3
lightofficialstudio <81398651+lightofficialstudio@users.noreply.github.com>
2024-12-31 22:54:38 +0700

    update gitignore

--- Dtos/Stock/CreateStockRequestDto.cs
+++ Dtos/Stock/CreateStockRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,22 @@ namespace api.Dtos.Stock
 {
     public class CreateStockRequestDto
     {
+        [Required(ErrorMessage = "Symbol is required.")]
         public string Symbol { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "CompanyName is required.")]
         public string CompanyName { get; set; } = string.Empty;
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Purchase must be greater than 0.")]
         public decimal Pruchase { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "LastDiv must be non-negative.")]
         public decimal LastDiv { get; set; }
+
+        [Required(ErrorMessage = "Industry is required.")]
         public string Industry { get; set; } = string.Empty;
+
+        [Range(1, long.MaxValue, ErrorMessage = "MarketCap must be greater than 0.")]
         public long MarketCap { get; set; }
     }
 }
--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce3da0eb248e5a880f704105d1c26baab219d51c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24866cf082772b886f02b0740f4894908b35927b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
